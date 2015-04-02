996e71aa12b0dcaa8029a8dfd0ad002b329bdcf5
Yang Ho Park <goodhobak@gmail.com>
2015-04-02 08:32:35 +0000

    test commit 2

--- public/app.js
+++
@@ -1,8 +0,0 @@
-var express=require('express');
-var app = express.createServer();
-app.get('/',function(req,res) {
-	res.send("hello node.js");
-});
-
-app.listen(process.env.PORT);
-console.log("Webserver Started on http://localhost:process.env.PORT: " + process.env.PORT);
